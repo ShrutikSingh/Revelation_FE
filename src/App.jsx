b59c712d3d8c1dac53fd86bcdde3350d71162af7
ShrutikSingh <shrutiksingh.26@gmail.com>
2025-03-04 02:38:26 +0530

    Setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2 className='text-xl font-bold text-red-500'>Hello This is React Vite+Tailwind</h2>
+    </>
+  )
+}
+
+export default App
