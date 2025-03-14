e4ad5781f3f8c86c5623fb8a4f223138ad1de3ad
cyber0knight <issacnewtonmukara@gmail.com>
2025-03-14 08:04:53 +0530

    resolved merge conflict in HomePage App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-<<<<<<< HEAD
-import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import EventPage from './pages/EventPage/EventPage'
-function App() {
-  
-=======
 import React from "react";
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import './App.css'
 
->>>>>>> 41c02c7bb194447b03deecb32885e8d075ffeea1
-
-
-
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Routes>
-        <Route path= "/eventpage" element = {<EventPage />} />
-      </Routes>  
-    </div>
-  )
-}
-=======
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path= "/eventpage" element = {<EventPage />} />
       </Routes>
     </Router>
   );
 };
->>>>>>> 41c02c7bb194447b03deecb32885e8d075ffeea1
 
 export default App;
