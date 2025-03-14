e9a2101afe975065fa3fcc41f6f483a1bffdff0a
cyber0knight <issacnewtonmukara@gmail.com>
2025-03-14 08:18:43 +0530

    [Bugfix in app.js]

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import './App.css'
 
 const App = () => {
   return (
-    <Router>
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path= "/eventpage" element = {<EventPage />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
